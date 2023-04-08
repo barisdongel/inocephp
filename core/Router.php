a3216d396fdebd06cda0c66ca8785432ff57fcc8
barisdongel <barisdng26@gmail.com>
2023-04-09 00:07:21 +0300

    view and layout

--- core/Router.php
+++ core/Router.php
@@ -4,7 +4,7 @@ namespace app\core;
 
 class Router
 {
-    public Request  $request;
+    public Request $request;
     protected array $routes = [];
 
     /**
@@ -26,10 +26,35 @@ class Router
         $path = $this->request->getPath();
         $method = $this->request->getMethod();
         $callback = $this->routes[$method][$path] ?? false;
-        if ($callback === false){
-            echo "Not Found";
-            exit();
+        if ($callback === false) {
+            return "Not Found";
         }
-        echo call_user_func($callback);
+        if (is_string($callback)) {
+            return $this->renderView($callback);
+        }
+        return call_user_func($callback);
     }
+
+    public function renderView($view)
+    {
+        $layoutContent = $this->layoutContent();
+        $viewContent = $this->renderOnlyView($view);
+        return str_replace('{{content}}', $viewContent, $layoutContent);
+        include_once Application::$ROOT_DIR . "/views/$view.php";
+    }
+
+    protected function layoutContent()
+    {
+        ob_start();
+        include_once Application::$ROOT_DIR . "/views/layouts/main.php";
+        return ob_get_clean();
+    }
+
+    protected function renderOnlyView($view)
+    {
+        ob_start();
+        include_once Application::$ROOT_DIR . "/views/$view.php";
+        return ob_get_clean();
+    }
+
 }
