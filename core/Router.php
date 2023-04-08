749553fda1d7e4913693e0bc4846fa6cd92fc593
barisdongel <barisdng26@gmail.com>
2023-04-08 23:33:15 +0300

    routing basics

--- core/Router.php
+++ core/Router.php
@@ -4,7 +4,18 @@ namespace app\core;
 
 class Router
 {
+    public Request  $request;
     protected array $routes = [];
+
+    /**
+     * @param Request $request
+     */
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+
     public function get($path, $callback)
     {
         $this->routes['get'][$path] = $callback;
@@ -12,6 +23,13 @@ class Router
 
     public function resolve()
     {
-
+        $path = $this->request->getPath();
+        $method = $this->request->getMethod();
+        $callback = $this->routes[$method][$path] ?? false;
+        if ($callback === false){
+            echo "Not Found";
+            exit();
+        }
+        echo call_user_func($callback);
     }
 }
